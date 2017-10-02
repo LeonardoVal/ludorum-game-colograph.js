5c917b24bc6eb8ec9b5ff26eae3823d9228ae8eb
Leonardo Val <leonardo.val@creatartis.com>
2017-10-02 10:51:24 -0300

    Playtester
    + Usable, except for a problem with webworker players.

---
+++ tests/playtester.js
@@ -0,0 +1,65 @@
+require(['creatartis-base', 'sermat', 'ludorum', 'ludorum-game-colograph', 'playtester'],
+function (base, Sermat, ludorum, ludorum_game_colograph, PlayTesterApp) {
+	window.base = base;
+	window.Sermat = Sermat;
+	window.ludorum = ludorum;
+	window.ludorum_game_colograph = ludorum_game_colograph;
+	window.PlayTesterApp = PlayTesterApp;
+
+	var BasicHTMLInterface = ludorum.players.UserInterface.BasicHTMLInterface;
+
+	/** Custom HTML interface for Othello.
+	*/
+	var ColographHTMLInterface = window.ColographHTMLInterface = base.declare(BasicHTMLInterface, {
+		constructor: function ColographHTMLInterface() {
+			BasicHTMLInterface.call(this, {
+				document: document,
+				container: document.getElementById('board')
+			});
+		},
+
+		display: function display(game) {
+			var ui = this,
+				activePlayer = game.activePlayer();
+			this.container.innerHTML = game.toSVG();
+			var nodes = this.container.children[0]
+				.querySelectorAll('.blank-node[data-ludorum-move]');
+			Array.prototype.slice.call(nodes).map(function (node) {
+					var move = +node.getAttribute('data-ludorum-move');
+					node.onclick = ui.perform.bind(ui, move, activePlayer);
+				});
+			return ui;
+		}
+	});
+
+	/** PlayTesterApp initialization.
+	*/
+	base.global.APP = new PlayTesterApp(
+		new ludorum_game_colograph.Colograph(),
+		new ColographHTMLInterface(),
+		{ bar: document.getElementsByTagName('footer')[0] },
+		[ludorum_game_colograph]
+	);
+	APP.change = function change() {
+		try {
+			this.game = ludorum_game_colograph.Colograph.randomGame();
+			this.reset();
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	APP.playerUI("You")
+		.playerRandom()
+		.playerMonteCarlo("", false, 10)
+		.playerMonteCarlo("", true, 100)
+		.playerUCT("", true, 10)
+		.playerUCT("", true, 100)
+		.playerAlfaBeta("MiniMax-\u03b1\u03b2 (4 plies)", true, 3)
+		.playerAlfaBeta("MiniMax-\u03b1\u03b2 (6 plies)", true, 5)
+		.selects(['player0', 'player1'])
+		.button('resetButton', document.getElementById('reset'), APP.reset.bind(APP))
+		.button('changeButton', document.getElementById('change'), APP.change.bind(APP))
+		.change();
+}, function (err) {
+	console.error(err);
+}); // require().
