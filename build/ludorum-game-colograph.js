09e4475084747ccb761157b04f318a048f89ce44
Leonardo Val <leonardo.val@creatartis.com>
2017-10-04 12:26:57 -0300

    New build

--- build/ludorum-game-colograph.js
+++ build/ludorum-game-colograph.js
@@ -133,8 +133,7 @@ var Colograph = exports.Colograph = declare(Game, {
 			'Invalid move: node ', move, ' does not exist in ', this, '.');
 		raiseIf(this.colours.hasOwnProperty(move),
 			'Invalid move: node ', move, ' has already been coloured in ', this, '.');
-		var newColours = Object.assign({}, this.colours);
-		newColours[move] = activePlayer;
+		var newColours = Object.assign(obj(move, activePlayer), this.colours);
 		this.edges[move].forEach(function (n2) { // Colour edges from the one coloured in this move.
 			if (newColours[n2] === activePlayer) {
 				newColours[move +','+ n2] = activePlayer;
@@ -190,24 +189,6 @@ var Colograph = exports.Colograph = declare(Game, {
 			colour1 = this.colours[node1],
 			colour2 = this.colours[node2];
 		return connected && colour1 >= 0 && colour1 === colour2 ? colour1 : -1;
-	},
-
-	// ## Heuristics. ##############################################################################
-
-	/** `heuristics` is a namespace for heuristic evaluation functions to be used with artificial
-	intelligence methods such as Minimax.
-	*/
-	'static heuristics': {
-		/** + `scoreDifference(game, player)` is a simple heuristic that uses the current score.
-		*/
-		scoreDifference: function scoreDifference(game, player) {
-			var score = game.score(),
-				result = 0;
-			for (var p in score) {
-				result += p === player ? score[p] : -score[p];
-			}
-			return result / game.edges.length / 2;
-		}
 	},
 
 	// ## Graph generation. ########################################################################
@@ -273,96 +254,160 @@ Sermat.include(exports);
 The user interface for playtesters is based on [SVG](https://www.w3.org/TR/SVG/).
 */
 
+Object.assign(Colograph.prototype, {
+
 /** Nodes in the `Colograph` object do not have a defined position. Yet they must be given one if
 the board is going to be rendered properly. This _arrangement_ methods calculate an array of
 positions for every node in the game, follwing different criteria:
 
 + `circularArrangement` puts all nodes in a circle of a given `radius`.
 */
-Colograph.prototype.circularArrangement = function circularArrangement(radius) {
-	radius = radius || 200;
-	var angle = 2 * Math.PI / this.edges.length;
-	return this.edges.map(function (adjs, n) {
-		return [Math.round(radius * Math.cos(angle * n)),
-			Math.round(radius * Math.sin(angle * n))];
-	});
-};
+	circularArrangement: function circularArrangement(radius) {
+		radius = radius || 200;
+		var angle = 2 * Math.PI / this.edges.length;
+		return this.edges.map(function (adjs, n) {
+			return [Math.round(radius * Math.cos(angle * n)),
+				Math.round(radius * Math.sin(angle * n))];
+		});
+	},
 
-//TODO More arrangement options.
+	//TODO More arrangement options.
 
 /**
 */
-var SVG = exports.SVG = {
-	HEADER: '<?xml version="1.0" standalone="no"?>\n'+
-		'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" '+
-			'"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
-	SHAPES: {
-		circle: '<circle id="circle-node" r="25px" cx="0" cy="0"/>',
-		square: '<rect id="square-node" width="50" height="50" x="-25" y="-25"/>',
-		star: '<polygon id="star-node" points="0,-24 6,-5 25,-5 10,5 15,24 0,13 -15,24 -10,5 -25,-5 -6,-5"/>',
-		triangle: '<polygon id="triangle-node" points="-25,22 0,-22 25,22"/>',
-		pentagon: '<polygon id="pentagon-node" points="0,-24 25,-5 15,24 -15,24 -25,-5"/>',
-		hexagon: '<polygon id="hexagon-node" points="0,-25 21,-12 21,12 0,25 -21,12 -21,-12"/>',
-	}
-};
+	__svgEnvelope__: function __svgEnvelope__(width, height, source) {
+		return '<?xml version="1.0" standalone="no"?>\n'+
+			'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" '+
+				'"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'+
+			'<svg height="'+ height +'px" width="'+ width +'px" version="1.1"\n'+
+				'xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n'+
+			(Array.isArray(source) ? source.join('\n\t') : source) +
+			'\n</svg>';
+	},
 
-Colograph.prototype.toSVG = function toSVG(width, height, positions) {
-	width = width || 500;
-	height = height || 500;
-	positions = positions || this.circularArrangement(Math.max(width, height) / 2.5);
-	var svg = [SVG.HEADER,
-		'<svg height="'+ height +'px" width="'+ width +'px" version="1.1"',
-		'\txmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">',
-		'\t<style type="text/css" ><![CDATA[',
-		'\t\t.blank-node { stroke:black; stroke-width:2px; fill:white; }',
-		'\t\t.blank-node:hover { stroke-width:4px; }',
-		'\t\t.blank-edge { stroke:black; stroke-width:2px; }'
-	];
-	this.players.forEach(function (p) { // Embedded CSS Styles
-		var colour = p.toLowerCase();
-		svg.push(
-			'\t\t.'+ colour +'-node { stroke:black; stroke-width:2px; fill:'+ colour +'}',
-			'\t\t.'+ colour +'-edge { stroke:'+ colour +'; stroke-width:2px }'
-		);
-	});
-	svg.push('\t]]></style>');
-	svg.push('\t<defs>'); // Shape library.
-	for (var shape in SVG.SHAPES) {
-		svg.push('\t\t'+ SVG.SHAPES[shape]);
-	}
-	svg.push('\t</defs>',
-		'\t<g id="colograph" transform="translate('+ (width/2) +','+ (height/2) +')">');
-	var colours = this.colours;
-	this.edges.forEach(function (n2s, n1) { // Draw edges.
-		var pos1 = positions[n1];
-		n2s.forEach(function (n2) {
-			var pos2 = positions[n2],
-				colour = colours[n1 +','+ n2],
-				cssClass = colour ? colour.toLowerCase() +'-edge' : 'blank-edge';
-			svg.push('\t<line class="'+ cssClass +'" data-edge="'+ JSON.stringify([pos1,pos2]) +
-				'" x1="'+ pos1[0] +'" y1="'+ pos1[1] +
-				'" x2="'+ pos2[0] +'" y2="'+ pos2[1] +'"/>');
+	__svgStyles__: function __svgStyles__() {
+		return '<style type="text/css" ><![CDATA[\n'+
+			'\t.blank-node { stroke:black; stroke-width:2px; fill:white; }\n'+
+			'\t.blank-node:hover { stroke-width:4px; }\n'+
+			'\t.blank-edge { stroke:black; stroke-width:2px; }\n'+
+			this.players.map(function (p) { // Embedded CSS Styles
+				var colour = p.toLowerCase();
+				return '\t.'+ colour +'-node { stroke:black; stroke-width:2px; fill:'+ colour +
+					'}\n\t.'+	colour +'-edge { stroke:'+ colour +'; stroke-width:2px }';
+			}).join('\n') +
+			'\t]]>\n\t</style>\n';
+	},
+
+	__svgDefs__: function __svgDefs__(nodeSize) {
+		nodeSize = nodeSize || 25;
+		return ['<defs>',
+			this.__svgDefCircle__(nodeSize),
+			this.__svgDefSquare__(nodeSize),
+			this.__svgDefStar__(nodeSize),
+			this.__svgDefTriangle__(nodeSize),
+			this.__svgDefPentagon__(nodeSize),
+			this.__svgDefHexagon__(nodeSize),
+			'</defs>'
+		].join('\n');
+	},
+
+	__svgDefCircle__: function __svgDefCircle__(nodeSize) {
+		return '\t<circle id="circle-node" r="'+ (nodeSize / 2) +'px" cx="0" cy="0"/>';
+	},
+
+	__svgDefSquare__: function __svgDefSquare__(nodeSize) {
+		return '\t<rect id="square-node" width="'+ nodeSize +'px" height="'+ nodeSize +'px"'+
+			' x="-'+ (nodeSize / 2) +'" y="-'+ (nodeSize / 2) +'"/>';
+	},
+
+	__svgDefPolygon__: function __svgDefPolygon__(name, points, nodeSize) {
+		points = points.map(function (p) {
+			return [p[0] * nodeSize, p[1] * nodeSize];
 		});
-	});
-	var shapes = this.shapes;
-	this.edges.forEach(function (adjs, n) { // Draw nodes.
-		var pos = positions[n],
-			colour = colours[n],
-			cssClass = colour ? colour.toLowerCase() +'-node' : 'blank-node';
-		svg.push('<use id="node'+ n +'" xlink:href="#'+ shapes[n] +'-node" '+
-			'transform="translate('+ pos.join(',') +')" class="'+ cssClass +'" '+
-			'data-ludorum-move="'+ n +'"/>');
-	});
-	svg.push('\t</g>', '</svg>');
-	return svg.join('\n');
-}; // Colograph.toSVG
+		return '\t<polygon id="'+ name +'-node" points="'+ points.join(' ') +'"/>';
+	},
 
-Colograph.prototype.ui = {
+	__svgDefTriangle__: function __svgDefTriangle__(nodeSize) {
+		return this.__svgDefPolygon__('triangle', [
+				[-0.50,+0.44], [+0.00,-0.44], [+0.50,+0.44]
+			], nodeSize);
+	},
+
+	__svgDefStar__: function __svgDefStar__(nodeSize) {
+		return this.__svgDefPolygon__('star', [
+				[+0.00,-0.48], [+0.12,-0.10], [+0.50,-0.10], [+0.20,+0.10], [+0.30,+0.48],
+				[+0.00,+0.26], [-0.30,+0.48], [-0.20,+0.10], [-0.50,-0.10], [-0.12,-0.10]
+			], nodeSize);
+	},
+
+	__svgDefPentagon__: function __svgDefPentagon__(nodeSize) {
+		return this.__svgDefPolygon__('pentagon', [
+				[+0.00,-0.48], [+0.50,-0.10], [+0.30,+0.48], [-0.30,+0.48], [-0.50,-0.10]
+			], nodeSize);
+	},
+
+	__svgDefHexagon__: function __svgDefHexagon__(nodeSize) {
+		return this.__svgDefPolygon__('pentagon', [
+				[+0.00,-0.50], [+0.42,-0.24], [+0.42,+0.24], [+0.00,+0.50], [-0.42,+0.24],
+				[-0.42,-0.24]
+			], nodeSize);
+	},
+
+	toSVG: function toSVG(width, height, nodeSize, positions) {
+		width = width || 400;
+		height = height || 400;
+		nodeSize = nodeSize || 30;
+		positions = positions || this.circularArrangement(Math.max(width, height) / 2.5);
+		var svg = [
+			this.__svgStyles__(),
+			this.__svgDefs__(nodeSize),
+			'\t<g id="colograph" transform="translate('+ (width/2) +','+ (height/2) +')">'
+		];
+		var colours = this.colours;
+		this.edges.forEach(function (n2s, n1) { // Draw edges.
+			var pos1 = positions[n1];
+			n2s.forEach(function (n2) {
+				var pos2 = positions[n2],
+					colour = colours[n1 +','+ n2],
+					cssClass = colour ? colour.toLowerCase() +'-edge' : 'blank-edge';
+				svg.push('\t<line class="'+ cssClass +'" x1="'+ pos1[0] +'" y1="'+ pos1[1] +
+					'" x2="'+ pos2[0] +'" y2="'+ pos2[1] +'"/>');
+			});
+		});
+		var shapes = this.shapes;
+		this.edges.forEach(function (adjs, n) { // Draw nodes.
+			var pos = positions[n],
+				colour = colours[n],
+				cssClass = colour ? colour.toLowerCase() +'-node' : 'blank-node';
+			svg.push('<use id="node'+ n +'" xlink:href="#'+ shapes[n] +'-node" '+
+				'transform="translate('+ pos.join(',') +')" class="'+ cssClass +'" '+
+				'data-ludorum-move="'+ n +'"/>');
+		});
+		svg.push('\t</g>');
+		return this.__svgEnvelope__(width, height, svg);
+	} // Colograph.toSVG
+}); //
 
 
+// # AI for Colograph.
 
+// ## Heuristics ###################################################################################
 
-}; // ui
+/** `heuristics` is a namespace for heuristic evaluation functions to be used with artificial
+intelligence methods such as Minimax.
+*/
+Colograph.heuristics = {
+	/** `scoreDifference(game, player)` is a simple heuristic that uses the current score.
+	*/
+	scoreDifference: function scoreDifference(game, player) {
+		var score = game.score(),
+			result = 0;
+		for (var p in score) {
+			result += p === player ? score[p] : -score[p];
+		}
+		return result / game.edges.length / 2;
+	}
+}; // Colograph.heuristics
 
 
 // See __prologue__.js
